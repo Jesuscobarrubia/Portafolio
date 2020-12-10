ce533ed56f05fa6d19d01a795f1c121a84137633
Jesus Cobarrubia <jesuscobarrubia18@gmail.com>
2020-12-10 15:19:36 -0300

    Portafolio Jesus Cobarrubia

---
+++ js/movimiento.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    // IR A LOS ELEMENTOS DEL MENU
+    $('#btn-acerca-de').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 400
+    }, 500);
+    })
+
+    $('#btn-trabajos').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 800
+    }, 500);
+    })
+
+    $('#btn-contacto').on('click', function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 1600
+    }, 500);
+    })
+});
